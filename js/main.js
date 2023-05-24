a2593204c4c0edfff061dab76fe57dff5676774c
Akktarus <zeitoun.anthony@gmail.com>
2023-05-24 17:47:59 +0200

    Commit final

--- js/main.js
+++ js/main.js
@@ -4,39 +4,35 @@ const image = document.getElementsByClassName("img");
 const val = document.getElementsByClassName("card-value");
 
 
-image[0].addEventListener("click", flip);
-image[1].addEventListener("click", flip);
-image[2].addEventListener("click", flip);
-image[3].addEventListener("click", flip);
-image[4].addEventListener("click", flip);
-image[5].addEventListener("click", flip);
-
-for (let i = 0; i < 5; i++) {
-    val[i] = 1 ;    
-    console.log("🚀 ~ file: main.js:16 ~ val[i]:", val[i])
+for (let i = 0; i < 6; i++) {
+    image[i].addEventListener("click", flip);
+    // image[i].addEventListener("click", reflip);
     
 }
 
 function flip(img) {
   let image = img.target;
   image.style.visibility = "hidden";
-  val[image.target]= 1;
+//   val[image.target]= 1;
   compare(image);
 }
 
-function compare(img) {
-  let count;
-  
-  for (let i = 0; i < 5; i++) {
-    
-    if (val[i] == 1 & img.style.visibility == "hidden") {
-        count += count;
-        console.log("🚀 ~ file: main.js:28 ~ compare ~ count:", count)
-        if (count == 2){
-        }else{
-            img.style.visibility = "visible";
-        }
-    }
-    
-  }
+function reflip(img) {
+    let image = img.target;
+    image.style.visibility = "visible"; 
 }
+
+// function compare(img) {
+//   let count;
+  
+//   for (let i = 0; i < 5; i++) {
+    
+//     if (val[i] == 1 & img.style.visibility == "hidden") {
+//         count += count;
+//         if (count == 2){
+//         }else{
+//             img.style.visibility = "visible";
+//         }
+//     }
+//   }
+// }
